7fae15842e3b9385e8f20c7bb59d257b42d6225b
Avircut <lomoff.slaw@yandex.ru>
2022-05-02 11:29:08 +0700

    header update and email php added

--- app/js/main.js
+++ app/js/main.js
@@ -87,7 +87,42 @@ $('a[href*="#"]')
 function validate(form){
 
 }
-$('.form').on("submit", (e) => {
-  let form = e.target;
-  console.log(form);
+const forms = document.querySelectorAll('.form');
+const regex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/
+forms.forEach((el) => {
+  const btn = el.querySelector('.form__btn');
+  btn.addEventListener('click', () => {
+    const inputs = el.elements;
+    const name = inputs['name'].value;
+    const phone = inputs['phone'].value;
+    let msg = el.querySelector('.form__validate-text');
+    if(!regex.test(phone) && name==''){
+      msg.innerHTML='Введите, пожалуйста, имя и номер телефона';
+    } else if(name==''){
+      msg.innerHTML='Введите, пожалуйста, имя';
+    }
+    else if(!regex.test(phone)){
+      msg.innerHTML='Введите, пожалуйста, корректный номер телефона';
+    }
+    else{
+    el.submit();
+    }
+  })
+})
+$('.form').on("submit", function() {
+  let form = $(this);
+  $.ajax({
+    url: 'email.php',
+    method:form.attr('method'),
+    data: form.serialize(),
+    success: function(result){
+      if(result == 'success'){
+        $('.form__validate-text').text('Успешно отправлено письмо');
+      }
+      else{
+        $('.form__validate-text').text('Произошла ошибка при отправке');
+      }
+    }
+  })
+  return false;
 })
